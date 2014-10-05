b6930f2d854062fe7bfa43205ab5c68f6ab1b126
Max Lynch <ihasmax@gmail.com>
2014-10-05 08:46:27 -0500

    Starter

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = express();
+
+app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
